d5d7543284ae609c62abbb67d8be8a5b9dbf0cec
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-26 15:04:11 +0200

    nix flake init -t github:hercules-ci/flake-parts

--- flake.nix
+++ flake.nix
@@ -1,55 +1,34 @@
 {
-  description = "Testing gepetto softwares";
+  description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { localSystem = system; };
-        hpp-fcl = pkgs.hpp-fcl.overrideAttrs {
-          src = pkgs.fetchFromGitHub {
-            owner = "humanoid-path-planner";
-            repo = "hpp-fcl";
-            rev = "65fb435b44a1bbd5059347d7a311cc7c7aa1349e";
-            fetchSubmodules = true;
-            hash = "sha256-qW5/rRvs0ZPWm9xQE+vf2cLc+0bd9KMW/NEI2i1hNvE=";
-          };
-        };
-        py-hpp-fcl = pkgs.python3Packages.toPythonModule (hpp-fcl.override { pythonSupport = true; });
-        pinocchio = pkgs.pinocchio.override { inherit hpp-fcl; };
-        py-pinocchio = pkgs.python3Packages.pinocchio.override {
-          inherit hpp-fcl;
-          python3Packages = {
-            inherit (pkgs.python3Packages) boost eigenpy;
-            hpp-fcl = py-hpp-fcl;
-          };
-        };
-        py-example-robot-data = pkgs.python3Packages.example-robot-data.override {
-          python3Packages = {
-            pinocchio = py-pinocchio;
-          };
-        };
-        crocoddyl = pkgs.crocoddyl.override { inherit pinocchio; };
-        py-crocoddyl = pkgs.python3Packages.crocoddyl.override {
-          inherit pinocchio;
-          python3Packages = {
-            inherit (pkgs.python3Packages) scipy;
-            pinocchio = py-pinocchio;
-            example-robot-data = py-example-robot-data;
-          };
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ py-crocoddyl ];
-          packages = [ (pkgs.python3.withPackages (ps: [ py-crocoddyl ])) ];
-        };
-      }
-    );
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        packages.default = pkgs.hello;
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
 }
