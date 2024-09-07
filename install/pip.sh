1b6a22d27029185daabffba4b0ef1f57f11faf40
Krzysztof Wojciechowski <49921081+Kotochleb@users.noreply.github.com>
2024-09-07 20:50:46 +0200

    Update versions of dependencies and pre-commit (#20)
    * Update JRL modules, pre-commit versions and dependencies
    
    * Update versions in readme
    
    ---------
    
    Co-authored-by: Krzysztof Wojciechowski <krzy.wojiecho@gmail.com>

--- install/pip.sh
+++ install/pip.sh
@@ -3,9 +3,9 @@
 rm -rf .venv
 python -m venv .venv
 source .venv/bin/activate
-pip install eigenpy[build]==3.4.0 cmeel-assimp cmeel-octomap cmeel-qhull
+pip install eigenpy[build]==3.5.1 cmeel-assimp cmeel-octomap cmeel-qhull
 pip install git+https://github.com/cmake-wheel/hpp-fcl.git@cmeel-devel
-pip install pin[build]==2.7.0 example-robot-data[build]==4.1.0 crocoddyl[build]==2.0.2 cmeel-mim-solvers[build]==0.0.4
+pip install pin[build]==2.7.0 example-robot-data[build]==4.1.0 crocoddyl[build]==2.1.0 cmeel-mim-solvers[build]==0.0.4
 
 CMAKE_PREFIX_PATH="$(cmeel cmake)"
 export CMAKE_PREFIX_PATH
