10d38b2db4b9f3a93caa53e14df43a9b130d2347
Krzysztof Wojciechowski <49921081+Kotochleb@users.noreply.github.com>
2024-09-11 11:22:00 +0200

    Feature/residual velocity avoidance (#24)
    * Implementation draft
    
    * Update CMake
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Clean up class data
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update forward declaration | cosmetic changes
    
    * Nearly compiling version
    
    * Compiling code
    
    * Remove overwrite flag
    
    * Add velocity avoidance parameters
    
    * Move parameters to model class
    
    * Add basic unit tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix pre-commit problems | Start implementing ddistdot_dq
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add sparse matrices
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Compiling code
    
    * Clean up matrix initialization | Remove accidental memory allocation
    
    * Add comments | Fix typo in code | Fix aliasing
    
    * Match results with new python implementation
    
    * Update scenes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove cout
    
    * Extend calcDiff test case
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Attempt on fixing deivatives
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- include/colmpc/residual-velocity-avoidance.hpp
+++ include/colmpc/residual-velocity-avoidance.hpp
@@ -1,13 +1,13 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (C) 2021, University of Edinburgh
+// Copyright (c) 2024, CNRS
 // Copyright note valid unless otherwise stated in individual files.
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef COLMPC_RESIDUAL_DISTANCE_COLLISION_HPP_
-#define COLMPC_RESIDUAL_DISTANCE_COLLISION_HPP_
+#ifndef COLMPC_RESIDUAL_VELOCITY_AVOIDANCE_HPP_
+#define COLMPC_RESIDUAL_VELOCITY_AVOIDANCE_HPP_
 
 #include "colmpc/fwd.hpp"
 // include fwd first
@@ -32,21 +32,32 @@ namespace colmpc {
 using namespace crocoddyl;
 
 template <typename _Scalar>
-struct ResidualDistanceCollisionTpl : public ResidualModelAbstractTpl<_Scalar> {
+struct ResidualModelVelocityAvoidanceTpl
+    : public ResidualModelAbstractTpl<_Scalar> {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   typedef _Scalar Scalar;
   typedef MathBaseTpl<Scalar> MathBase;
   typedef ResidualModelAbstractTpl<Scalar> Base;
-  typedef ResidualDataDistanceCollisionTpl<Scalar> Data;
+  typedef ResidualDataVelocityAvoidanceTpl<Scalar> Data;
   typedef ResidualDataAbstractTpl<Scalar> ResidualDataAbstract;
   typedef StateMultibodyTpl<Scalar> StateMultibody;
   typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
   typedef pinocchio::GeometryModel GeometryModel;
   typedef typename MathBase::VectorXs VectorXs;
   typedef typename MathBase::MatrixXs MatrixXs;
+  typedef typename MathBase::MatrixXs Matrix3s;
   typedef typename MathBase::Matrix6xs Matrix6xs;
   typedef typename MathBase::Vector3s Vector3s;
+  typedef Eigen::DiagonalMatrix<Scalar, 3> DiagonalMatrix3s;
+  typedef Eigen::Matrix<Scalar, 12, 1> Vector12s;
+  typedef Eigen::Matrix<Scalar, 8, 8> Matrix8s;
+  typedef Eigen::Matrix<Scalar, 8, 6> Matrix86s;
+  typedef Eigen::Matrix<Scalar, 3, 6> Matrix36s;
+  typedef Eigen::Matrix<Scalar, 3, 12> Matrix312s;
+  typedef Eigen::Matrix<Scalar, 12, 12> Matrix1212s;
+  typedef Eigen::Matrix<Scalar, 6, -1> Matrix6xLike;
+  typedef Eigen::Matrix<Scalar, 12, -1> Matrix12xLike;
   /**
    * @brief Initialize the pair collision residual model
    *
@@ -55,13 +66,19 @@ struct ResidualDistanceCollisionTpl : public ResidualModelAbstractTpl<_Scalar> {
    * @param[in] geom_model  Pinocchio geometry model containing the collision
    * pair
    * @param[in] pair_id     Index of the collision pair in the geometry model
+   * @param[in] di          Distance at which the robot starts to slow down
+   * @param[in] ds          Security distance
+   * @param[in] ksi         Convergence speed coefficient
    */
 
-  ResidualDistanceCollisionTpl(boost::shared_ptr<StateMultibody> state,
-                               const std::size_t nu,
-                               boost::shared_ptr<GeometryModel> geom_model,
-                               const pinocchio::PairIndex pair_id);
-  virtual ~ResidualDistanceCollisionTpl();
+  ResidualModelVelocityAvoidanceTpl(boost::shared_ptr<StateMultibody> state,
+                                    const std::size_t nu,
+                                    boost::shared_ptr<GeometryModel> geom_model,
+                                    const pinocchio::PairIndex pair_id,
+                                    const Scalar di = 1.0e-2,
+                                    const Scalar ds = 1.0e-5,
+                                    const Scalar ksi = 1.0e-2);
+  virtual ~ResidualModelVelocityAvoidanceTpl();
 
   /**
    * @brief Compute the pair collision residual
@@ -91,7 +108,7 @@ struct ResidualDistanceCollisionTpl : public ResidualModelAbstractTpl<_Scalar> {
   /**
    * @brief Return the Pinocchio geometry model
    */
-  const pinocchio::GeometryModel &get_geometry() const;
+  const GeometryModel &get_geometry() const;
 
   /**
    * @brief Return the reference collision pair id
@@ -103,6 +120,36 @@ struct ResidualDistanceCollisionTpl : public ResidualModelAbstractTpl<_Scalar> {
    */
   void set_pair_id(const pinocchio::PairIndex pair_id);
 
+  /**
+   * @brief Return the distance at which the robot starts to slow down
+   */
+  Scalar get_di() const;
+
+  /**
+   * @brief Modify the distance at which the robot starts to slow down
+   */
+  void set_di(const Scalar di);
+
+  /**
+   * @brief Return the security distance
+   */
+  Scalar get_ds() const;
+
+  /**
+   * @brief Modify the security distance
+   */
+  void set_ds(const Scalar ds);
+
+  /**
+   * @brief Return the convergence speed coefficient
+   */
+  Scalar get_ksi() const;
+
+  /**
+   * @brief Modify the convergence speed coefficient
+   */
+  void set_ksi(const Scalar ksi);
+
  protected:
   using Base::nu_;
   using Base::state_;
@@ -112,19 +159,18 @@ struct ResidualDistanceCollisionTpl : public ResidualModelAbstractTpl<_Scalar> {
  private:
   typename StateMultibody::PinocchioModel
       pin_model_;  //!< Pinocchio model used for internal computations
-  boost::shared_ptr<pinocchio::GeometryModel>
+  boost::shared_ptr<GeometryModel>
       geom_model_;  //!< Pinocchio geometry model containing collision pair
   pinocchio::PairIndex
       pair_id_;  //!< Index of the collision pair in geometry model
-  pinocchio::JointIndex joint_id_;  //!< Index of joint on which the collision
-                                    //!< body frame of the robot is attached
 
-  // const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic>
-  // q;
+  Scalar di_;   //!< Distance at which the robot starts to slow down
+  Scalar ds_;   //!< Security distance
+  Scalar ksi_;  //!< Convergence speed coefficient
 };
 
 template <typename _Scalar>
-struct ResidualDataDistanceCollisionTpl
+struct ResidualDataVelocityAvoidanceTpl
     : public ResidualDataAbstractTpl<_Scalar> {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -134,19 +180,39 @@ struct ResidualDataDistanceCollisionTpl
   typedef StateMultibodyTpl<Scalar> StateMultibody;
   typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
 
+  typedef pinocchio::DataTpl<Scalar> PinocchioData;
   typedef typename MathBase::VectorXs VectorXs;
   typedef typename MathBase::Matrix6xs Matrix6xs;
+  typedef typename MathBase::Matrix3s Matrix3s;
   typedef typename MathBase::Vector3s Vector3s;
+  typedef Eigen::Matrix<Scalar, 8, 8> Matrix8s;
+  typedef Eigen::Matrix<Scalar, 8, 6> Matrix86s;
+  typedef Eigen::Matrix<Scalar, 3, 6> Matrix36s;
+  typedef Eigen::Matrix<Scalar, 6, Eigen::Dynamic> Matrix6xLike;
+  typedef Eigen::Matrix<Scalar, 12, Eigen::Dynamic> Matrix12xLike;
 
   template <template <typename Scalar> class Model>
-  ResidualDataDistanceCollisionTpl(Model<Scalar> *const model,
+  ResidualDataVelocityAvoidanceTpl(Model<Scalar> *const model,
                                    DataCollectorAbstract *const data)
       : Base(model, data),
         geometry(pinocchio::GeometryData(model->get_geometry())),
         req(),
         res(),
-        J1(6, model->get_state()->get_nv()),
-        J2(6, model->get_state()->get_nv()) {
+        ddistdot_dq_val(model->get_state()->get_nv() * 2),
+        d_dist_dot_dq(model->get_state()->get_nv()),
+        J(model->get_state()->get_nv()),
+        q(model->get_state()->get_nv()),
+        v(model->get_state()->get_nv()),
+        __a(model->get_state()->get_nv()),
+        d_theta1_dq(6, model->get_state()->get_nv()),
+        d_theta2_dq(6, model->get_state()->get_nv()),
+        jacobian1(6, model->get_state()->get_nv()),
+        jacobian2(6, model->get_state()->get_nv()),
+        d_theta1_dot_dq(6, model->get_state()->get_nv()),
+        d_theta2_dot_dq(6, model->get_state()->get_nv()),
+        __dv(6, model->get_state()->get_nv()),
+        d_theta_dq(12, model->get_state()->get_nv()),
+        d_theta_dot_dq(12, model->get_state()->get_nv()) {
     // Check that proper shared data has been passed
     DataCollectorMultibodyTpl<Scalar> *d =
         dynamic_cast<DataCollectorMultibodyTpl<Scalar> *>(shared);
@@ -155,18 +221,41 @@ struct ResidualDataDistanceCollisionTpl
           "Invalid argument: the shared data should be derived from "
           "DataCollectorActMultibodyTpl");
     }
+
     // Avoids data casting at runtime
     pinocchio = d->pinocchio;
 
-    J1.fill(0);
-    J2.fill(0);
-    cp1.fill(0);
-    cp2.fill(0);
-    f1p1.fill(0);
-    f2p2.fill(0);
+    ddistdot_dq_val.setZero();
+    d_dist_dot_dq.setZero();
+    J.setZero();
+    q.setZero();
+    v.setZero();
+    __a.setZero();
+    d_theta1_dq.setZero();
+    d_theta2_dq.setZero();
+    jacobian1.setZero();
+    jacobian2.setZero();
+    d_theta1_dot_dq.setZero();
+    d_theta2_dot_dq.setZero();
+    __dv.setZero();
+    d_theta_dq.setZero();
+    d_theta_dot_dq.setZero();
+
+    f1Mp1.setIdentity();
+    f2Mp2.setIdentity();
+
+    // Set values to the parts of the matrix that never change
+    Lyy.setZero();
+    Lyy.template block<3, 3>(0, 3) = -Matrix3s::Identity();
+    Lyy.template block<3, 3>(3, 3) = -Matrix3s::Identity();
+    Lyy.template block<3, 3>(3, 0) = -Matrix3s::Identity();
+
+    Lyc.setZero();
+    Lyr.setZero();
   }
   pinocchio::GeometryData geometry;       //!< Pinocchio geometry data
   pinocchio::DataTpl<Scalar> *pinocchio;  //!< Pinocchio data
+
   using Base::r;
   using Base::Ru;
   using Base::Rx;
@@ -177,19 +266,36 @@ struct ResidualDataDistanceCollisionTpl
             //!< used to compute the distance between shapes
   hpp::fcl::DistanceResult res;  //!< Distance Result from hppfcl
 
-  Matrix6xs J1;
-  Matrix6xs J2;
-  Vector3s cp1;
-  Vector3s cp2;
-
-  Vector3s f1p1;
-  pinocchio::SE3 f1Mp1;
-
-  Vector3s f2p2;
-  pinocchio::SE3 f2Mp2;
-
   pinocchio::SE3 oMg_id_1;
   pinocchio::SE3 oMg_id_2;
+
+  pinocchio::SE3 f1Mp1;
+  pinocchio::SE3 f2Mp2;
+
+  Scalar distance;
+
+  pinocchio::Motion m1;
+  pinocchio::Motion m2;
+
+  VectorXs ddistdot_dq_val;
+  VectorXs d_dist_dot_dq;
+  VectorXs J;
+  VectorXs q;
+  VectorXs v;
+  VectorXs __a;
+  Matrix6xLike d_theta1_dq;
+  Matrix6xLike d_theta2_dq;
+  Matrix6xLike jacobian1;
+  Matrix6xLike jacobian2;
+  Matrix6xLike d_theta1_dot_dq;
+  Matrix6xLike d_theta2_dot_dq;
+  Matrix6xLike __dv;
+  Matrix12xLike d_theta_dq;
+  Matrix12xLike d_theta_dot_dq;
+
+  Matrix8s Lyy;
+  Matrix86s Lyc;
+  Matrix86s Lyr;
 };
 
 }  // namespace colmpc
@@ -197,5 +303,5 @@ struct ResidualDataDistanceCollisionTpl
 /* --- Details -------------------------------------------------------------- */
 /* --- Details -------------------------------------------------------------- */
 /* --- Details -------------------------------------------------------------- */
-#include "colmpc/residual-distance-collision.hxx"
-#endif  // COLMPC_RESIDUAL_DISTANCE_COLLISION_HPP_
+#include "colmpc/residual-velocity-avoidance.hxx"
+#endif  // COLMPC_RESIDUAL_VELOCITY_AVOIDANCE_HPP_
---
+++ include/colmpc/residual-velocity-avoidance.hxx
@@ -0,0 +1,355 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (c) 2024, CNRS
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef COLMPC_RESIDUAL_VELOCITY_AVOIDANCE_HXX_
+#define COLMPC_RESIDUAL_VELOCITY_AVOIDANCE_HXX_
+#ifdef PINOCCHIO_WITH_HPP_FCL
+
+#include "colmpc/residual-velocity-avoidance.hpp"
+
+namespace colmpc {
+using namespace crocoddyl;
+
+template <typename Scalar>
+ResidualModelVelocityAvoidanceTpl<Scalar>::ResidualModelVelocityAvoidanceTpl(
+    boost::shared_ptr<StateMultibody> state, const std::size_t nu,
+    boost::shared_ptr<GeometryModel> geom_model,
+    const pinocchio::PairIndex pair_id, const Scalar di, const Scalar ds,
+    const Scalar ksi)
+    : Base(state, 1, nu, true, true, true),
+      pin_model_(*state->get_pinocchio()),
+      geom_model_(geom_model),
+      pair_id_(pair_id),
+      di_(di),
+      ds_(ds),
+      ksi_(ksi) {
+  if (static_cast<pinocchio::FrameIndex>(geom_model_->collisionPairs.size()) <=
+      pair_id) {
+    throw_pretty(
+        "Invalid argument: "
+        << "the pair index is wrong (it does not exist in the geometry model)");
+  }
+}
+
+template <typename Scalar>
+ResidualModelVelocityAvoidanceTpl<
+    Scalar>::~ResidualModelVelocityAvoidanceTpl() {}
+
+template <typename Scalar>
+void ResidualModelVelocityAvoidanceTpl<Scalar>::calc(
+    const boost::shared_ptr<ResidualDataAbstract> &data,
+    const Eigen::Ref<const VectorXs> &, const Eigen::Ref<const VectorXs> &) {
+  Data *d = static_cast<Data *>(data.get());
+
+  // clear the hppfcl results
+  d->res.clear();
+
+  const auto &cp = geom_model_->collisionPairs[pair_id_];
+  const auto &geom_1 = geom_model_->geometryObjects[cp.first];
+  const auto &geom_2 = geom_model_->geometryObjects[cp.second];
+  const pinocchio::Model::JointIndex joint_id_1 = geom_1.parentJoint;
+  const pinocchio::Model::JointIndex joint_id_2 = geom_2.parentJoint;
+
+  // Update geometry placement
+  if (joint_id_1 > 0) {
+    d->oMg_id_1 = d->pinocchio->oMi[joint_id_1] * geom_1.placement;
+  } else {
+    d->oMg_id_1 = geom_1.placement;
+  }
+
+  if (joint_id_2 > 0) {
+    d->oMg_id_2 = d->pinocchio->oMi[joint_id_2] * geom_2.placement;
+  } else {
+    d->oMg_id_2 = geom_2.placement;
+  }
+
+  // compute distance between geometries
+  d->distance = hpp::fcl::distance(
+      geom_1.geometry.get(), toFclTransform3f(d->oMg_id_1),
+      geom_2.geometry.get(), toFclTransform3f(d->oMg_id_2), d->req, d->res);
+
+  // compute velocity
+  d->m1 =
+      pinocchio::getFrameVelocity(pin_model_, *d->pinocchio, geom_1.parentFrame,
+                                  pinocchio::LOCAL_WORLD_ALIGNED);
+  d->m2 =
+      pinocchio::getFrameVelocity(pin_model_, *d->pinocchio, geom_2.parentFrame,
+                                  pinocchio::LOCAL_WORLD_ALIGNED);
+
+  const Vector3s &x1 = d->res.nearest_points[0];
+  const Vector3s &x2 = d->res.nearest_points[1];
+  const Vector3s &c1 = d->oMg_id_1.translation();
+  const Vector3s &c2 = d->oMg_id_2.translation();
+
+  Vector3s Lc = x2 - x1;
+  const Vector3s Lr1 = c1.transpose() * pinocchio::skew(Lc) +
+                       x2.transpose() * pinocchio::skew(x1);
+
+  // Negate Lc inverting order of substraction to x1 - x2
+  Lc.noalias() = -1.0 * Lc;
+  const Vector3s Lr2 = c2.transpose() * pinocchio::skew(Lc) +
+                       x1.transpose() * pinocchio::skew(x2);
+
+  const Scalar Ldot = Lc.dot(d->m1.linear() - d->m2.linear()) +
+                      Lr1.dot(d->m1.angular()) + Lr2.dot(d->m2.angular());
+  d->r[0] = (Ldot / d->distance) + ksi_ * (d->distance - ds_) / (di_ - ds_);
+}
+
+template <typename Scalar>
+void ResidualModelVelocityAvoidanceTpl<Scalar>::calcDiff(
+    const boost::shared_ptr<ResidualDataAbstract> &data,
+    const Eigen::Ref<const VectorXs> &x, const Eigen::Ref<const VectorXs> &) {
+  Data *d = static_cast<Data *>(data.get());
+
+  const std::size_t nq = pin_model_.nq;
+  d->q = x.head(nq);
+  d->v = x.tail(nq);
+
+  // Create labels for geometries
+  const auto &cp = geom_model_->collisionPairs[pair_id_];
+  const auto &geom_1 = geom_model_->geometryObjects[cp.first];
+  const auto &geom_2 = geom_model_->geometryObjects[cp.second];
+  // Crate labels for points
+  const Vector3s &x1 = d->res.nearest_points[0];
+  const Vector3s &x2 = d->res.nearest_points[1];
+  const Vector3s &c1 = d->oMg_id_1.translation();
+  const Vector3s &c2 = d->oMg_id_2.translation();
+  // Create labels for velocities
+  const Vector3s &v1 = d->m1.linear();
+  const Vector3s &v2 = d->m2.linear();
+  const Vector3s &w1 = d->m1.angular();
+  const Vector3s &w2 = d->m2.angular();
+  // Convert sphere representation to a diagonal matrix
+  const DiagonalMatrix3s D1 =
+      std::static_pointer_cast<hpp::fcl::Ellipsoid>(geom_1.geometry)
+          ->radii.asDiagonal();
+  const DiagonalMatrix3s D2 =
+      std::static_pointer_cast<hpp::fcl::Ellipsoid>(geom_2.geometry)
+          ->radii.asDiagonal();
+
+  pinocchio::computeForwardKinematicsDerivatives(pin_model_, *d->pinocchio,
+                                                 d->q, d->v, d->__a);
+
+  // Store rotations of geometries in matrices
+  const Matrix3s &R1 = d->oMg_id_1.rotation();
+  const Matrix3s &R2 = d->oMg_id_2.rotation();
+
+  const Matrix3s A1 = R1 * D1 * R1.transpose();
+  const Matrix3s A2 = R2 * D2 * R2.transpose();
+
+  // Precompute differences as they are often used;
+  const Vector3s x_diff = x1 - x2;
+  const Vector3s x1_c1_diff = x1 - c1;
+  const Vector3s x2_c2_diff = x2 - c2;
+  // Precompute cross products as they are used many times
+  const Vector3s x_diff_cross_x1_c1_diff = x_diff.cross(x1_c1_diff);
+  const Vector3s x_diff_cross_x2_c2_diff = x_diff.cross(x2_c2_diff);
+
+  const Scalar sol_lam1 = -x1_c1_diff.dot(x_diff);
+  const Scalar sol_lam2 = x2_c2_diff.dot(x_diff);
+
+  const Scalar Ldot = x_diff.dot(v1 - v2) - x_diff_cross_x1_c1_diff.dot(w1) +
+                      x_diff_cross_x2_c2_diff.dot(w2);
+
+  // Precompute inverse od distance for faster operations
+  const Scalar distance_inv = 1.0 / d->distance;
+  const Scalar dist_dot = Ldot * distance_inv;
+
+  // Precompute components of the matrix that are repeating
+  const Vector3s A1_x1_c1_diff = A1 * x1_c1_diff;
+  const Vector3s A2_x2_c2_diff = A2 * x2_c2_diff;
+  const Matrix3s sol_lam1_A1 = sol_lam1 * A1;
+  const Matrix3s sol_lam2_A2 = sol_lam2 * A2;
+  const Matrix3s x1_c1_diff_skew = pinocchio::skew(x1_c1_diff);
+  const Matrix3s x2_c2_diff_skew = pinocchio::skew(x2_c2_diff);
+  // Fill Lyy matrix only with the blocks that are changing
+  d->Lyy.template topLeftCorner<3, 3>().noalias() =
+      Matrix3s::Identity(3, 3) + sol_lam1_A1;
+  d->Lyy.template block<3, 1>(0, 6).noalias() = A1_x1_c1_diff;
+  d->Lyy.template block<3, 3>(3, 3).noalias() =
+      Matrix3s::Identity(3, 3) + sol_lam2_A2;
+  d->Lyy.template block<3, 1>(3, 7).noalias() = A2_x2_c2_diff;
+  d->Lyy.template block<1, 3>(6, 0).noalias() = A1_x1_c1_diff;
+  d->Lyy.template block<1, 3>(7, 3).noalias() = A2_x2_c2_diff;
+
+  // Fill Lyc matrix only with the blocks that are changing
+  d->Lyc.template topLeftCorner<3, 3>().noalias() = -sol_lam1_A1;
+  d->Lyc.template block<3, 3>(3, 3).noalias() = -sol_lam2_A2;
+  d->Lyc.template block<1, 3>(6, 0).noalias() = -A1_x1_c1_diff;
+  d->Lyc.template bottomRightCorner<1, 3>().noalias() = -A2_x2_c2_diff;
+
+  // Fill Lyr matrix only with the blocks that are changing
+  d->Lyr.template topLeftCorner<3, 3>().noalias() =
+      sol_lam1 * (A1 * x1_c1_diff_skew - pinocchio::skew(A1_x1_c1_diff));
+  d->Lyr.template block<3, 3>(3, 3).noalias() =
+      sol_lam2 * (A2 * x2_c2_diff_skew - pinocchio::skew(A2_x2_c2_diff));
+  d->Lyr.template block<1, 3>(6, 0).noalias() =
+      x1_c1_diff.transpose() * A1 * x1_c1_diff_skew;
+  d->Lyr.template bottomRightCorner<1, 3>().noalias() =
+      x2_c2_diff.transpose() * A2 * x2_c2_diff_skew;
+
+  // TODO find better matrix inversion
+  const Matrix8s Lyy_inv = -1.0 * d->Lyy.inverse();
+  const Matrix86s yc = Lyy_inv * d->Lyc;
+  const Matrix86s yr = Lyy_inv * d->Lyr;
+
+  const Matrix312s dx1 =
+      (Matrix312s() << yc.template topRows<3>(), yr.template topRows<3>())
+          .finished();
+  const Matrix312s dx2 = (Matrix312s() << yc.template middleRows<3>(3),
+                          yr.template middleRows<3>(3))
+                             .finished();
+
+  // Precompute difference of vectors
+  const Matrix312s dx_diff = dx1 - dx2;
+
+  const Vector12s dL_dtheta =
+      (Vector12s() << x_diff, -x_diff, -x_diff_cross_x1_c1_diff,
+       x_diff_cross_x2_c2_diff)
+          .finished();
+
+  const Matrix3s x_diff_skew = pinocchio::skew(x_diff);
+
+  Matrix1212s ddL_dtheta2;
+  // Initialize matrix
+  ddL_dtheta2 << dx_diff, -dx_diff,
+      -x_diff_skew * dx1 + x1_c1_diff_skew * dx_diff,
+      x_diff_skew * dx2 - x2_c2_diff_skew * dx_diff;
+  // Update only certain blocks of the matrix
+  // Aliasing occurs but is harmless in this case
+  ddL_dtheta2.template block<3, 3>(6, 0).noalias() =
+      ddL_dtheta2.template block<3, 3>(6, 0) + x_diff_skew;
+  ddL_dtheta2.template block<3, 3>(9, 3).noalias() =
+      ddL_dtheta2.template block<3, 3>(9, 3) - x_diff_skew;
+
+  // Compute (1 / distance)^2
+  const Scalar distance_inv_pow = distance_inv * distance_inv;
+  const Vector12s theta_dot = (Vector12s() << v1, v2, w1, w2).finished();
+  const Vector12s d_dist_dot_dtheta =
+      ((theta_dot.transpose() * ddL_dtheta2) * distance_inv).transpose() -
+      (dist_dot * distance_inv_pow * dL_dtheta);
+
+  pinocchio::computeFrameJacobian(
+      pin_model_, *d->pinocchio, d->q, geom_1.parentFrame,
+      pinocchio::LOCAL_WORLD_ALIGNED, d->d_theta1_dq);
+  pinocchio::computeFrameJacobian(
+      pin_model_, *d->pinocchio, d->q, geom_2.parentFrame,
+      pinocchio::LOCAL_WORLD_ALIGNED, d->d_theta2_dq);
+
+  // Assign valued to preallocated d_theta_dq matrix
+  d->d_theta_dq.template topRows<3>().noalias() =
+      d->d_theta1_dq.template topRows<3>();
+  d->d_theta_dq.template middleRows<3>(3).noalias() =
+      d->d_theta2_dq.template topRows<3>();
+  d->d_theta_dq.template middleRows<3>(6).noalias() =
+      d->d_theta1_dq.template bottomRows<3>();
+  d->d_theta_dq.template bottomRows<3>().noalias() =
+      d->d_theta2_dq.template bottomRows<3>();
+
+  pinocchio::getFrameVelocityDerivatives(pin_model_, *d->pinocchio,
+                                         geom_1.parentFrame, pinocchio::WORLD,
+                                         d->d_theta1_dot_dq, d->__dv);
+  pinocchio::getFrameVelocityDerivatives(pin_model_, *d->pinocchio,
+                                         geom_2.parentFrame, pinocchio::WORLD,
+                                         d->d_theta2_dot_dq, d->__dv);
+
+  // Rewrite data to d_theta_dot_dq vector. Only rows 0:3 and 6:9 are non zero
+  d->d_theta_dot_dq.template topRows<3>().noalias() =
+      d->d_theta1_dot_dq.template topRows<3>();
+  d->d_theta_dot_dq.template middleRows<3>(3).noalias() =
+      d->d_theta2_dot_dq.template topRows<3>();
+  d->d_theta_dot_dq.template middleRows<3>(6).noalias() =
+      d->d_theta1_dot_dq.template bottomRows<3>();
+  d->d_theta_dot_dq.template bottomRows<3>().noalias() =
+      d->d_theta2_dot_dq.template bottomRows<3>();
+
+  const Vector12s d_dist_dot_dtheta_dot =
+      distance_inv * theta_dot.transpose() * ddL_dtheta2;
+  d->d_dist_dot_dq.noalias() =
+      d_dist_dot_dtheta.transpose() * d->d_theta_dq +
+      d_dist_dot_dtheta_dot.transpose() * d->d_theta_dot_dq;
+
+  // Transport the jacobian of frame 1 into the jacobian associated to x1
+  const Vector3s &p1 = d->pinocchio->oMf[geom_1.parentFrame].translation();
+  d->f1Mp1.translation(x1 - p1);
+  d->jacobian1.noalias() = d->f1Mp1.toActionMatrixInverse() * d->d_theta1_dq;
+
+  const Vector3s &p2 = d->pinocchio->oMf[geom_2.parentFrame].translation();
+  d->f2Mp2.translation(x2 - p2);
+  d->jacobian2.noalias() = d->f2Mp2.toActionMatrixInverse() * d->d_theta2_dq;
+
+  d->J.noalias() =
+      distance_inv * x_diff.transpose() *
+      (d->jacobian1.template topRows<3>() - d->jacobian2.template topRows<3>());
+
+  d->ddistdot_dq_val.topRows(nq).noalias() =
+      d->d_dist_dot_dq - (d->J * ksi_ / (di_ - ds_));
+  d->ddistdot_dq_val.bottomRows(nq).noalias() = d->J;
+
+  d->Rx.noalias() = d->ddistdot_dq_val;
+}
+template <typename Scalar>
+boost::shared_ptr<ResidualDataAbstractTpl<Scalar> >
+ResidualModelVelocityAvoidanceTpl<Scalar>::createData(
+    DataCollectorAbstract *const data) {
+  return boost::allocate_shared<Data>(Eigen::aligned_allocator<Data>(), this,
+                                      data);
+}
+
+template <typename Scalar>
+const pinocchio::GeometryModel &
+ResidualModelVelocityAvoidanceTpl<Scalar>::get_geometry() const {
+  return *geom_model_.get();
+}
+
+template <typename Scalar>
+pinocchio::PairIndex ResidualModelVelocityAvoidanceTpl<Scalar>::get_pair_id()
+    const {
+  return pair_id_;
+}
+
+template <typename Scalar>
+void ResidualModelVelocityAvoidanceTpl<Scalar>::set_pair_id(
+    const pinocchio::PairIndex pair_id) {
+  pair_id_ = pair_id;
+}
+
+template <typename Scalar>
+Scalar ResidualModelVelocityAvoidanceTpl<Scalar>::get_di() const {
+  return di_;
+}
+
+template <typename Scalar>
+void ResidualModelVelocityAvoidanceTpl<Scalar>::set_di(const Scalar di) {
+  di_ = di;
+}
+
+template <typename Scalar>
+Scalar ResidualModelVelocityAvoidanceTpl<Scalar>::get_ds() const {
+  return ds_;
+}
+
+template <typename Scalar>
+void ResidualModelVelocityAvoidanceTpl<Scalar>::set_ds(const Scalar ds) {
+  ds_ = ds;
+}
+
+template <typename Scalar>
+Scalar ResidualModelVelocityAvoidanceTpl<Scalar>::get_ksi() const {
+  return ksi_;
+}
+
+template <typename Scalar>
+void ResidualModelVelocityAvoidanceTpl<Scalar>::set_ksi(const Scalar ksi) {
+  ksi_ = ksi;
+}
+
+}  // namespace colmpc
+
+#endif  // PINOCCHIO_WITH_HPP_FCL
+#endif  // COLMPC_RESIDUAL_VELOCITY_AVOIDANCE_HXX_
