4f75ddee72096b4091dacede5b0155dbba01b318
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-04-23 18:18:41 +0200

    Update the nix build using the gepetto overlay. Issues with the py-colmpc and the crocoddyl version.

--- flake.nix
+++ flake.nix
@@ -2,49 +2,62 @@
   description = "Collision avoidance for MPC";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Remove once the next mim-solvers release reach nixpkgs/nixos-unstable
-    mim-solvers = {
-      # Move to master once https://github.com/machines-in-motion/mim_solvers/pull/48 is merged.
-      url = "github:machines-in-motion/mim_solvers/main";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-      };
-    };
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      systems = [ "x86_64-linux" ];
+      imports = [ inputs.treefmt-nix.flakeModule ];
       perSystem =
-        { pkgs, self', system, ... }:
+        {
+          lib,
+          pkgs,
+          system,
+          self',
+          ...
+        }:
         {
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
             overlays = [
-              (final: prev: {
-                mim-solvers = prev.mim-solvers.overrideAttrs (super: {
-                  src = inputs.mim-solvers;
-                  postPatch = ""; # disable default patch
-                });
-              })
+              inputs.nix-ros-overlay.overlays.default
+              inputs.gepetto.overlays.default
             ];
           };
-          apps.default = {
-            type = "app";
-            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-          };
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
-          packages = {
-            default = self'.packages.py-colmpc;
-            colmpc = pkgs.callPackage ./. { };
-            py-colmpc = pkgs.python3Packages.toPythonModule (
-              self'.packages.colmpc.override { pythonSupport = true; }
-            );
+          checks = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
+          packages =
+            let
+              src = lib.fileset.toSource {
+                root = ./.;
+                fileset = lib.fileset.unions [
+                  ./examples
+                  ./include
+                  ./python
+                  ./tests
+                  ./CMakeLists.txt
+                  ./package.xml
+                  ./pyproject.toml
+                ];
+              };
+            in
+            {
+              default = self'.packages.py-colmpc;
+              colmpc =
+                pkgs.colmpc.overrideAttrs
+                  { inherit src; };
+              py-colmpc = pkgs.python3Packages.colmpc.overrideAttrs {
+                inherit src;
+              };
+            };
+          treefmt.programs = {
+            deadnix.enable = true;
+            nixfmt.enable = true;
           };
         };
     };
