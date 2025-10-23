7b353c7f97ed3fe59df615aba0f43e56824a10f7
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-23 16:41:31 +0200

    flake: clean

--- flake.nix
+++ flake.nix
@@ -25,29 +25,26 @@
         {
           packages =
             let
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./examples
-                  ./include
-                  ./python
-                  ./tests
-                  ./CMakeLists.txt
-                  ./package.xml
-                  ./pyproject.toml
-                ];
+              override = {
+                src = lib.fileset.toSource {
+                  root = ./.;
+                  fileset = lib.fileset.unions [
+                    ./examples
+                    ./include
+                    ./python
+                    ./tests
+                    ./CMakeLists.txt
+                    ./package.xml
+                    ./pyproject.toml
+                  ];
+                };
+                patches = [ ];
               };
             in
             {
               default = self'.packages.py-colmpc;
-              colmpc = pkgs.colmpc.overrideAttrs {
-                inherit src;
-                patches = [ ];
-              };
-              py-colmpc = pkgs.python3Packages.colmpc.overrideAttrs {
-                inherit src;
-                patches = [ ];
-              };
+              colmpc = pkgs.colmpc.overrideAttrs override;
+              py-colmpc = pkgs.python3Packages.colmpc.overrideAttrs override;
             };
         };
     };
