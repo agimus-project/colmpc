f2f521df5b70f12d292814adde9830c0fd6c3fb5
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-13 01:59:57 +0100

    nix: switch to gazebros2nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Collision avoidance for MPC";
 
   inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
+    gazebros2nix.url = "github:gepetto/gazebros2nix";
+    flake-parts.follows = "gazebros2nix/flake-parts";
+    nixpkgs.follows = "gazebros2nix/nixpkgs";
+    nix-ros-overlay.follows = "gazebros2nix/nix-ros-overlay";
+    systems.follows = "gazebros2nix/systems";
+    treefmt-nix.follows = "gazebros2nix/treefmt-nix";
   };
 
   outputs =
@@ -17,8 +17,8 @@
       {
         systems = import inputs.systems;
         imports = [
-          inputs.gepetto.flakeModule
-          { gepetto-pkgs.overlays = [ self.overlays.default ]; }
+          inputs.gazebros2nix.flakeModule
+          { gazebros2nix-pkgs.overlays = [ self.overlays.default ]; }
         ];
         flake.overlays.default = _final: prev: {
           colmpc = prev.colmpc.overrideAttrs {
@@ -41,7 +41,7 @@
           {
             packages = {
               default = self'.packages.colmpc;
-              colmpc = pkgs.python3Packages.colmpc.override { standalone = false; };
+              colmpc = pkgs.python3Packages.colmpc.override { buildStandalone = false; };
             };
           };
       }
