e38814f82abc9c72111c06fd82fc9e73d042317d
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-24 18:02:53 +0100

    flake: update gazebros2nix use

--- flake.nix
+++ flake.nix
@@ -15,34 +15,34 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } (
       { lib, self, ... }:
       {
-        systems = import inputs.systems;
+        systems = [ "x86_64-linux" ];
         imports = [
           inputs.gazebros2nix.flakeModule
-          { gazebros2nix-pkgs.overlays = [ self.overlays.default ]; }
-        ];
-        flake.overlays.default = _final: prev: {
-          colmpc = prev.colmpc.overrideAttrs {
-            src = lib.fileset.toSource {
-              root = ./.;
-              fileset = lib.fileset.unions [
-                ./examples
-                ./include
-                ./python
-                ./tests
-                ./CMakeLists.txt
-                ./package.xml
-                ./pyproject.toml
-              ];
-            };
-          };
-        };
-        perSystem =
-          { pkgs, self', ... }:
           {
-            packages = {
-              default = self'.packages.colmpc;
-              colmpc = pkgs.python3Packages.colmpc.override { buildStandalone = false; };
+            gazebros2nix = {
+              packages = {
+                colmpc = _final: {
+                  src = lib.fileset.toSource {
+                    root = ./.;
+                    fileset = lib.fileset.unions [
+                      ./examples
+                      ./include
+                      ./python
+                      ./tests
+                      ./CMakeLists.txt
+                      ./package.xml
+                      ./pyproject.toml
+                    ];
+                  };
+                };
+              };
             };
+          }
+        ];
+        perSystem =
+          { pkgs, ... }:
+          {
+            packages.default = pkgs.python3Packages.colmpc;
           };
       }
     );
